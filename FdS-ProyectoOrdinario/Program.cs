86e1a333ae9c562435a61aee2c985f2180656ba3
Forever Park <140984288+foreverpark@users.noreply.github.com>
2023-12-07 09:30:32 -0600

    Fix creation memes

---
+++ FdS-ProyectoOrdinario/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FdS_ProyectoOrdinario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
