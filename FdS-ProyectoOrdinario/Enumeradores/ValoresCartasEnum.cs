7eb3cfa32d75e6b47ceead950b4b707edc17ff37
foreverpark <yoshimario771@gmail.com>
2023-12-08 08:15:47 -0600

    Añadir interfaces y enumeradores del proyecto
    Interfaces y enumeradores de Duarte.

---
+++ FdS-ProyectoOrdinario/Enumeradores/ValoresCartasEnum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoOrdinario.Enumeradores
+{
+    public enum ValoresCartasEnum
+    {
+        As = 1,
+        Dos,
+        Tres,
+        Cuatro,
+        Cinco,
+        Seis,
+        Siete,
+        Ocho,
+        Nueve,
+        Diez,
+        Jota,
+        Reina,
+        Rey
+    }
+}
