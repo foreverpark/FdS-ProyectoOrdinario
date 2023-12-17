cbccfcf9f2f3b2ed55657bfab018de30829ecf6e
Eliam28 <152578969+Eliam28@users.noreply.github.com>
2023-12-16 22:31:57 -0600

    Implementaciion Menu de la consola
    Se implemento un switch para jugar los siguientes juegos y se implemento la parte de seleccionar el numero de jugadores

--- FdS-ProyectoOrdinario/Program.cs
+++ FdS-ProyectoOrdinario/Program.cs
@@ -1,5 +1,6 @@
 ﻿using ProyectoOrdinario.Interfaces;
 using ProyectoOrdinario.Enumeradores;
+using FdS_ProyectoOrdinario.Modelos.Blackjack;
 
 namespace FdS_ProyectoOrdinario
 {
@@ -7,8 +8,55 @@ namespace FdS_ProyectoOrdinario
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world");
-            
+
+            Console.WriteLine("¿Que juego desea jugar?  1) Poker clásico  2) 21 BlackJack");
+            int OpcionJuego=int.Parse(Console.ReadLine());
+
+            switch (OpcionJuego)
+            {
+                case 1: 
+                    {
+                        Console.WriteLine("Estas jugando Poker Clásico :)");
+                        break;
+                    
+                    
+                    }
+
+
+                case 2: 
+                    {
+                        Console.WriteLine("\nEstas jugando 21 Black Jack :)\n");
+                        int NumeroJugadores = 0;
+                        Console.WriteLine("Ingrese el numero de Jugadores: ");
+                        while (true) 
+                        {
+                            NumeroJugadores = int.Parse(Console.ReadLine());
+
+                            if(NumeroJugadores >0 && NumeroJugadores < 8)
+                            { 
+                                break;
+                            }
+                            else 
+                            {
+                                Console.WriteLine("\nIngrese un numero mayor que 0 y menor que 8");
+                            }
+                        }
+
+                        var juegoBlackJack = new JuegoBlackjack();
+
+
+
+
+                        break;
+                    }
+
+                    default: 
+                    {
+
+                        Console.WriteLine("Ingrese un numero Valido");
+                        break;
+                    }
+            }
         }
     }
 }
