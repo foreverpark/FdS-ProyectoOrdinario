c81324e45c9fe0fcefe9df2ff82861f489f81461
foreverpark <yoshimario771@gmail.com>
2023-12-18 04:25:42 -0600

    Poker - Implementar juego
    Da excepción de Sin Implementar cuando un jugador realiza su jugada.

--- FdS-ProyectoOrdinario/Program.cs
+++ FdS-ProyectoOrdinario/Program.cs
@@ -1,6 +1,7 @@
 ﻿using ProyectoOrdinario.Interfaces;
 using ProyectoOrdinario.Enumeradores;
 using FdS_ProyectoOrdinario.Modelos.Blackjack;
+using FdS_ProyectoOrdinario.Modelos.Poker;
 using System.ComponentModel;
 
 namespace FdS_ProyectoOrdinario
@@ -16,13 +17,35 @@ namespace FdS_ProyectoOrdinario
             {
                 case 1: 
                     {
+                        var juegoPoker = new JuegoPoker();
                         Console.WriteLine("Estas jugando Poker Clásico :)");
-                        break;
-                    
-                    
+                        int NumeroJugadores = 0;
+                        Console.WriteLine("Ingrese el numero de Jugadores: ");
+                        while (true)
+                        {
+                            NumeroJugadores = int.Parse(Console.ReadLine());
+
+                            //validate number
+                            if (NumeroJugadores > 0 && NumeroJugadores < 8)
+                            {
+                                Console.Clear();
+                                break;
+                            }
+                            else
+                            {
+                                Console.WriteLine("\nIngrese un numero mayor que 0 y menor que 8");
+                            }
+                        }
+
+                        for (int i = 0; i < NumeroJugadores; i++)
+                        {
+                            juegoPoker.AgregarJugador(new JugadorPoker());
+                        }
+
+                        juegoPoker.IniciarJuego();
+                        
                     }
-
-
+                    break;
                 case 2: 
                     {
                         Console.WriteLine("\nEstas jugando 21 Black Jack :)\n");
